d88da6eae5c898996b0dc2b365c474d764a45f43
SiHun Sung <sungsihun@gmail.com>
2018-08-22 10:56:18 -0400

    initial

---
+++ boozeME/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  boozeME
+//
+//  Created by NICE on 2018-08-22.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ boozeME/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  boozeME
+//
+//  Created by NICE on 2018-08-22.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
